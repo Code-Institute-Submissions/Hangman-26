a97fa0124d66b0e9a76bcee193253b34b7898e5a
Iris Smok <iriscemeljic11@gmail.com>
2022-01-04 13:24:19 +0000

    Add welcome title functiona and clear terminal function

--- run.py
+++ run.py
@@ -1,11 +1,43 @@
+"""
+Project Portfolio
+"""
 import random
 import word
+import os
+import sys
 
 easy_level = word.easy_words
 medium_level = word.med_words
 hard_level = word.hard_words
 
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def welcome():
+    """
+    Display a welcome title and navigate to start the game or see rules
+    """
+    clear_terminal()
+    print('{:*^80}'.format(' WELCOME TO HANGMAN ! '))
+    print('\n' * 3)
+    print('{:^80}'.format(' 1: PLAY GAME '))
+    print('{:^80}'.format(' 2: SEE RULES '))
+    print('\n' * 4)
+    while True:
+        player_choice = input('Please select 1 to play the game or 2 to see the rules.\n')
+        if player_choice == '1':
+            print('hello')
+        elif player_choice == '2':
+            rules()
+
+
 def rules():
+    """
+    Display rules after the title
+    """
+    clear_terminal()
     print("To play the game you must guess the letters of the hidden word.")
     print("If the guess is correct the letter missing in the word")
     print("is replaced by the correct letter.")
@@ -16,11 +48,18 @@ def rules():
     print("Be careful because a harder level means more letters.")
     print("Good luck!\n")
 
+    menu = input("Press enter to return to the main menu")
+    welcome()
+
+welcome()
+
+
+
 def display_hangman(lives):
     """
     Hangman lives
     """
-    stages = [ 
+    stages = [
                 """
                 _________
                 |/      |
@@ -111,10 +150,9 @@ def display_hangman(lives):
             ___________
                 """,
                 """
-                
-                
-                
-                
+
+
+
                 """
 
     ]
