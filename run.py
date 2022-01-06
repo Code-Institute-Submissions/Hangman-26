d53ffad0e9019d8cd43f81dabf7ae58c7302d95a
Iris Smok <iriscemeljic11@gmail.com>
2022-01-06 09:49:14 +0000

    Move title to the middle of the console

--- run.py
+++ run.py
@@ -8,19 +8,26 @@ hard_level = word.hard_words
 
 
 def clear_terminal():
+    """"
+    Clear terminal
+    """
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
 def title():
+    """
+    Display the title
+    """
     print('{:*^80}'.format(
-        """
-         _   _
-        | | | | __ _ _ __   __ _ _ __ ___   __ _ _ __
-        | |_| |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\
-        |  _  | (_| | | | | (_| | | | | | | (_| | | | |
-        |_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
-                            |___/
-        """
-    ))
+                """
+                 _   _
+                | | | | __ _ _ __   __ _ _ __ ___   __ _ _ __
+                | |_| |/ _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\
+                |  _  | (_| | | | | (_| | | | | | | (_| | | | |
+                |_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                                    |___/
+                """
+            ))
 
 
 def welcome():
@@ -34,7 +41,8 @@ def welcome():
     print('{:^80}'.format(' 2 SEE RULES '))
     print('\n' * 4)
     while True:
-        player_choice = input('Please select 1 to play the game or 2 to see the rules.\n')
+        player_choice = input('Please select 1 to play the game\
+             or 2 to see the rules.\n')
         if player_choice == '1':
             print('hello')
         elif player_choice == '2':
@@ -47,23 +55,26 @@ def rules():
     """
     clear_terminal()
     title()
-    print('\n' * 4)
-    print('{:^80}'.format("To play the game you must guess the letters of the hidden word."))
-    print('{:^80}'.format("If the guess is correct the letter missing in the word"))
+    print('\n' * 2)
+    print('{:^80}'.format("To play the game you must guess the\
+                          letters of the hidden word."))
+    print('{:^80}'.format("If the guess is correct the\
+                          letter missing in the word"))
     print('{:^80}'.format("is replaced by the correct letter."))
-    print('{:^80}'.format("You can enter the whole word if you now what the word is."))
+    print('{:^80}'.format("You can enter the whole word if\
+                           you now what the word is."))
     print('{:^80}'.format("Each wrong guess takes one life."))
-    print('{:^80}'.format("You can choose the dificulty level, E for easy (10 lives)"))
-    print('{:^80}'.format("M for medium (8 lives) or H for hard (6 lives)"))
-    print('{:^80}'.format("Be careful because a harder level means more letters."))
+    print('{:^80}'.format("You can choose the dificulty level,\
+                           E for easy (10 lives)"))
+    print('{:^80}'.format("M for medium (8 lives) or\
+                           H for hard (6 lives)"))
+    print('{:^80}'.format("Be careful because a harder level\
+                          means more letters."))
     print('{:^80}'.format("Good luck!\n"))
     print('\n' * 2)
 
     menu = input("Press enter to return to the main menu\n")
     welcome()
-
-
-welcome()
 
 
 def display_hangman(lives):
@@ -169,7 +180,12 @@ def display_hangman(lives):
     ]
     return stages[lives]
 
+
 def main():
+    """
+    Main function
+    """
     welcome()
+
 
 main()
